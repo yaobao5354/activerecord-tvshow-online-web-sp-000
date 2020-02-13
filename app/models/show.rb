5aad53d3476e9d37238c403b14135e126a1c52fd
yaoyu bao <yaobao5354@gmail.com>
2020-02-13 18:58:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,22 @@ require 'pry'
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maxmium
+    Show.order(:rating).last.rating
   end
 
+  def self.most_popular_show
+    Show.order(:rating).last
+  end
+
+  def self.lowest_rating
+    Show.order(:rating).first.rating
+  end
+
+  def self.least_popular_show
+    Show.order(:rating).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end 
 end
